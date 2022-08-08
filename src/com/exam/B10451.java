00f8a3e37bffff019c17159ce2a9b9dd393c719b
dwc04112 <dwc04112@gmail.com>
2022-08-08 15:59:53 +0900

    순열사이클 https://www.acmicpc.net/problem/10451

---
+++ src/com/exam/B10451.java
@@ -0,0 +1,35 @@
+package com.exam;
+
+import java.util.Scanner;
+
+public class B10451 {
+    static int[] intArr;
+    static boolean[] visited;
+
+
+    static int dfs(int j){
+        if(j ==  intArr[j] || visited[j]) return 1;
+        visited[j] = true;
+        return dfs(intArr[j]);
+    }
+
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        int N  = sc.nextInt();
+
+        for(int i=0; i<N; i++){
+            int M = sc.nextInt();
+            intArr = new int[M+1];
+            visited = new boolean[M+1];
+            int count = 0;
+
+            for(int j=1; j<M+1; j++){
+               intArr[j] = sc.nextInt();
+            }
+            for(int j=1; j<M+1; j++){
+                if(!visited[j]) count += dfs(j);
+            }
+            System.out.println(count);
+        }
+    }
+}
