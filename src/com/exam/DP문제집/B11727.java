360fd5c0d54c17a0d8d39793ece2329c0ee498fc
dwc04112 <dwc04112@gmail.com>
2022-09-07 19:45:39 +0900

    2xn 타일링2  https://www.acmicpc.net/problem/11727 blog :: https://dwc04112.tistory.com/230

---
+++ src/com/exam/DP문제집/B11727.java
@@ -0,0 +1,18 @@
+package com.exam.DP문제집;
+
+import java.util.Scanner;
+
+public class B11727 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] dp = new int[N+2];
+        dp[1] = 1;
+        dp[2] = 3;
+        for (int i = 3; i <= N; i++) {
+          //  dp[i] = (dp[i - 1] + (dp[i - 2] * 2)) % 10007;
+            dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % 10007;
+        }
+        System.out.println(dp[N]);
+    }
+}
