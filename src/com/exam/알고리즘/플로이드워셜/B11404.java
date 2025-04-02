275f48bed4a7b36dd56fe6a17f397549fb2e9484
mz-yuseungyeop <yuseungyeop@mz.co.kr>
2025-04-02 15:33:34 +0900

    플로이드 https://www.acmicpc.net/problem/11404

---
+++ src/com/exam/알고리즘/플로이드워셜/B11404.java
@@ -0,0 +1,60 @@
+package com.exam.알고리즘.플로이드워셜;
+
+import java.util.Scanner;
+
+
+public class B11404 {
+    static int N;
+    static int C;
+    static int IFN = 999999;
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+        N = sc.nextInt();
+        C = sc.nextInt();
+
+        int[][] nd = new int[N+1][N+1];
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if(i==j) {
+                    nd[i][j] = 0;
+                    continue;
+                }
+                nd[i][j] = IFN;
+            }
+        }
+
+        for (int i = 1; i <= C; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            int cost = sc.nextInt();
+
+            nd[x][y] = Math.min(nd[x][y], cost);
+        }
+
+        for (int n = 1; n <= N; n++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+
+                    if(nd[i][j] > nd[i][n] + nd[n][j]) {
+                        nd[i][j] = nd[i][n] + nd[n][j];
+                    }
+                }
+            }
+        }
+        print(nd);
+    }
+
+    public static void print(int[][] board) {
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if(i==N && j==N) {
+                    System.out.print(board[i][j] == IFN ? 0 : board[i][j]);
+                    continue;
+                }
+                System.out.print(board[i][j] == IFN ? 0 + " " : board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
