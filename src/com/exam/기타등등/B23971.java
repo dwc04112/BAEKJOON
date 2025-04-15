ab7691c6e0edc11ab28639f798372bedbf48220b
mz-yuseungyeop <yuseungyeop@mz.co.kr>
2025-04-15 11:27:51 +0900

    ZOAC 4 https://www.acmicpc.net/problem/23971

---
+++ src/com/exam/기타등등/B23971.java
@@ -0,0 +1,18 @@
+package com.exam.기타등등;
+
+import java.util.Scanner;
+
+public class B23971 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int H = sc.nextInt();
+        int W = sc.nextInt();
+        int y = sc.nextInt();
+        int x = sc.nextInt();
+
+        int hy = (int) Math.ceil((double) H / (1+y));
+        int wx = (int) Math.ceil((double) W / (1+x));
+
+        System.out.println(hy*wx);
+    }
+}
